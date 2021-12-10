2d9571ca0eaf04be397f5c70904cb14accecd363
joseflekardal <joseflekardal@gmail.com>
2021-12-10 14:45:35 +0100

    feat: added day 10

---
+++ 2021/day10.1.js
@@ -0,0 +1,30 @@
+const input = process.argv[2];
+
+const tagMap = { "[": "]", "{": "}", "(": ")", "<": ">" };
+const prices = { ")": 3, "]": 57, "}": 1197, ">": 25137 };
+const closingTags = new Set(Object.keys(prices));
+const tagMemo = {};
+
+for (const sequence of input.split("\n")) {
+  let lastOpening = [];
+
+  for (const tag of sequence) {
+    if (closingTags.has(tag)) {
+      if (tag !== tagMap[lastOpening[0]]) {
+        tagMemo[tag] = tagMemo[tag] ? tagMemo[tag] + 1 : 1;
+        break;
+      } else {
+        lastOpening.shift();
+      }
+    } else {
+      lastOpening.unshift(tag);
+    }
+  }
+}
+
+const result = Object.entries(tagMemo).reduce(
+  (tot, [tag, count]) => tot + prices[tag] * count,
+  0
+);
+
+console.log(result);
---
+++ 2021/day10.2.js
@@ -0,0 +1,32 @@
+const input = process.argv[2];
+
+const tagMap = { "[": "]", "{": "}", "(": ")", "<": ">" };
+const priceMap = { ")": 1, "]": 2, "}": 3, ">": 4 };
+const closingTags = new Set(Object.keys(priceMap));
+const tagMemo = {};
+
+const scores = input.split("\n").reduce((scores, sequence) => {
+  let lastOpening = [];
+
+  for (const tag of sequence) {
+    if (closingTags.has(tag)) {
+      if (tag !== tagMap[lastOpening[0]]) {
+        tagMemo[tag] = tagMemo[tag] ? tagMemo[tag] + 1 : 1;
+        return scores;
+      } else {
+        lastOpening.shift();
+      }
+    } else {
+      lastOpening.unshift(tag);
+    }
+  }
+
+  scores.push(lastOpening.reduce((a, tag) => a * 5 + priceMap[tagMap[tag]], 0));
+
+  return scores;
+}, []);
+
+const sortedScores = scores.sort((a, b) => b - a);
+const meanScore = sortedScores[Math.floor(scores.length / 2)];
+
+console.log(meanScore);
