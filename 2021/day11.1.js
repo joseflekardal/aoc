546cc4bb8f760570d1377f5cb134bde5e6a45bfb
joseflekardal <joseflekardal@gmail.com>
2021-12-13 10:01:42 +0100

    feat: add day 11

---
+++ 2021/day11.1.js
@@ -0,0 +1,62 @@
+const input = process.argv[2]
+
+const grid = input.split('\n').map((row) => {
+  return Array.from(row).map(Number)
+})
+
+const OCTOPUS_COUNT = grid.flat().length
+let flashedCount = 0
+let step = 0
+
+while (step < OCTOPUS_COUNT) {
+  step++
+  const flashed = []
+
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      grid[y][x]++
+      if (grid[y][x] > 9) {
+        flashed.push([y, x])
+      }
+    }
+  }
+
+  const flashedOctoMemo = flashed.reduce((memo, pos) => {
+    const positionsToCheck = [pos]
+
+    while (positionsToCheck.length) {
+      const [y, x] = positionsToCheck.pop()
+      const gridValue = grid[y]?.[x]
+
+      if (!gridValue) {
+        continue
+      }
+
+      grid[y][x]++
+
+      const memoKey = [y, x].join(':')
+      if (!memo.has(memoKey) && grid[y][x] > 9) {
+        memo.add(memoKey)
+        grid[y][x] = 0
+
+        positionsToCheck.push(
+          [y + 1, x],
+          [y - 1, x],
+          [y, x - 1],
+          [y, x + 1],
+          [y - 1, x - 1],
+          [y + 1, x + 1],
+          [y - 1, x + 1],
+          [y + 1, x - 1]
+        )
+        continue
+      }
+    }
+
+    return memo
+  }, new Set())
+
+  flashedCount += flashedOctoMemo.size
+}
+
+console.log(flashedCount)
