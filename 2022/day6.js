69d664379dc5199cf9761aff944887c15a89ec95
Josef Lekardal <joseflekardal@gmail.com>
2022-12-06 22:04:37 +0100

    feat: add solution for 2022 day 6

---
+++ 2022/day6.js
@@ -0,0 +1,25 @@
+function solvePuzzle(input, sequenceLength = 4) {
+	let state = []
+	let result = 0
+
+	for (let i = 0; i < input.length; i++) {
+		const char = input[i]
+		const index = state.indexOf(char)
+		const hasChar = index !== -1
+
+		if (hasChar) {
+			state = state.slice(index + 1)
+		}
+
+		state.push(char)
+
+		if (state.length === sequenceLength) {
+			result = i + 1
+			break
+		}
+	}
+}
+
+const input = document.querySelector('pre')
+const result = solvePuzzle(input)
+console.log(result)
